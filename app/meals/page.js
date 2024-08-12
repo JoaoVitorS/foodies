cc3a532f54c6a99d9a0a033e5dae53142376bb91
Joao Vitor <joao@192.168.0.2>
2024-08-12 12:52:48 -0300

    feat: finished starting page

---
+++ app/meals/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function MealsPage() {
+  return (
+    <main>
+      <div>MealsPage</div>
+      <p><Link href="/meals/share">Go to share meals page</Link></p>
+    </main>
+  )
+}
